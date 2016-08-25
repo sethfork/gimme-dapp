3dff3e9153a33b2d6216251fffb3cba3e8bcda00
Cody Burns <cody.w.burns@gmail.com>
2016-08-25 17:35:22 -0500

    Create index.js

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var mongoose = require( 'mongoose' );
+var Block     = mongoose.model( 'Block' );
+
+//var Memcached = require('memcached');
+//var memcached = new Memcached("localhost:11211");
+
+module.exports = function(app){
+  if (app.get('env') === 'development')
+    var web3relay = require('./web3dummy');
+  else
+    var web3relay = require('./web3relay');
+
+  app.post('/web3relay', web3relay.data);
+
+}
